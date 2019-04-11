027d06d850b34a35f4f276c6bddee8d9309ef728
Mathieu Colin <Mathieu.Colin@them-is.fr>
2019-04-11 16:20:21 +0200

    commit

--- src/process/ProcessUtils.ts
+++ src/process/ProcessUtils.ts
@@ -1,7 +1,7 @@
 class ProcessUtils {
-  static isProd = (): boolean => process.env && process.env.NODE_ENV === "production";
+  static isProd = (): boolean => true; // process.env && process.env.NODE_ENV === "production";
 
-  static isDev = (): boolean => process.env && process.env.NODE_ENV === "development";
+  static isDev = (): boolean => true; // process.env && process.env.NODE_ENV === "development";
 }
 
 export default ProcessUtils;
