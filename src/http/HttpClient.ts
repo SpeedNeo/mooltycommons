4fc46daceaf0d173b58d946868aa6fe2025b16a0
Mathieu Colin <Mathieu.Colin@them-is.fr>
2019-04-17 16:11:04 +0200

    commit

--- src/http/HttpClient.ts
+++ src/http/HttpClient.ts
@@ -3,13 +3,16 @@ import Client from "../Client";
 const POST: string = "POST";
 const PUT: string = "PUT";
 
+type FunctionResponse = (response: Response) => void;
 type FunctionSuccess<T> = (json: T) => void;
 type FunctionError = (error: any) => void;
 
 class HttpClient implements Client {
-  getResponseJson = <T>(url: string, fctSuccess: FunctionSuccess<T> = () => {}, fctError: FunctionError = () => {}): void => {
+  getResponseJson = <T>(url: string, fctResponse: FunctionResponse = () => {}, fctSuccess: FunctionSuccess<T> = () => {}, fctError: FunctionError = () => {}): void => {
     fetch(url)
       .then((response: Response) => {
+        fctResponse(response);
+
         if (response.ok) {
           return response.json();
         }
@@ -18,7 +21,7 @@ class HttpClient implements Client {
       .catch(error => fctError(error));
   }
 
-  methodJsonResponseJson = <U, V>(method: string, url: string, data: U, fctSuccess: FunctionSuccess<V>, fctError: FunctionError): void => {
+  methodJsonResponseJson = <U, V>(method: string, url: string, data: U, fctResponse: FunctionResponse, fctSuccess: FunctionSuccess<V>, fctError: FunctionError): void => {
     fetch(url, {
       method: method,
       headers: {
@@ -28,19 +31,19 @@ class HttpClient implements Client {
       body: JSON.stringify(data)
     })
       .then((response: Response) => {
+        fctResponse(response);
+
         if (response.ok) {
           return response.json();
         }
-
-        throw new Error(response.statusText);
       })
       .then(json => fctSuccess(json))
       .catch(error => fctError(error));
   }
 
-  postJsonResponseJson = <U, V>(url: string, data: U, fctSuccess: FunctionSuccess<V> = () => {}, fctError: FunctionError = () => {}) => this.methodJsonResponseJson(POST, url, data, fctSuccess, fctError);
+  postJsonResponseJson = <U, V>(url: string, data: U, fctResponse: FunctionResponse = () => {}, fctSuccess: FunctionSuccess<V> = () => {}, fctError: FunctionError = () => {}) => this.methodJsonResponseJson(POST, url, data, fctResponse, fctSuccess, fctError);
 
-  putJsonResponseJson = <U, V>(url: string, data: U, fctSuccess: FunctionSuccess<V> = () => {}, fctError: FunctionError = () => {}) => this.methodJsonResponseJson(PUT, url, data, fctSuccess, fctError);
+  putJsonResponseJson = <U, V>(url: string, data: U, fctResponse: FunctionResponse = () => {}, fctSuccess: FunctionSuccess<V> = () => {}, fctError: FunctionError = () => {}) => this.methodJsonResponseJson(PUT, url, data, fctResponse, fctSuccess, fctError);
 
   shutDown = () => {};
 }
